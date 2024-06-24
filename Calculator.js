55228d3634fc73dbf525a7c2b6d8369ba04ef2c1
Mihir Sai Dudekula <114400284+MihirSaiDudekula@users.noreply.github.com>
2024-06-24 16:12:34 +0530

    Add files via upload
    this is not the correct repo but for now it can stay
    
    i want to do infix to prefix and then evaluate

---
+++ Calculator.js
@@ -0,0 +1,15 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (e) {
+        document.getElementById('display').value = 'Error';
+    }
+}
